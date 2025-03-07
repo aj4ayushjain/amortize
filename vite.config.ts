ea50c59335310a1c5f39f2406d8fe16d340d7791
Ayush Jain <ayushjain@Ayushs-MacBook-Pro.local>
2025-03-07 21:39:39 +0530

    Perf: Optimize js minification and enable tree-shaking
    Signed-off-by: Ayush Jain <ayushjain@Ayushs-MacBook-Pro.local>

--- vite.config.ts
+++ vite.config.ts
@@ -16,4 +16,10 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    minify: 'terser', // 'terser' if you need better optimization
+    rollupOptions: {
+      treeshake: true,
+    },
+  },
 })
