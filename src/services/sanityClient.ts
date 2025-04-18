2a613a3cf2914f751795e7bb78c3ca6d3345688c
Ayush Jain <aj4ayushjain@gmail.com>
2025-04-19 00:14:16 +0530

    Update vars - vite supported
    Signed-off-by: Ayush Jain <aj4ayushjain@gmail.com>

--- src/services/sanityClient.ts
+++ src/services/sanityClient.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@sanity/client';
 
 const sanityClient = createClient({
-  projectId: process.env.REACT_APP_SANITY_PROJECT_ID || '', // Fetch from .env
-  dataset: process.env.REACT_APP_SANITY_DATASET || 'production', // Fetch from .env
+  projectId: import.meta.env.VITE_SANITY_PROJECT_ID || '', // Fetch from .env
+  dataset: import.meta.env.VITE_SANITY_DATASET || 'production', // Fetch from .env
   apiVersion: '2023-01-01',    // Use the latest API version
   useCdn: true,                // `true` for faster, cached responses
 });
