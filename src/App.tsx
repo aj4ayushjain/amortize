e667c207c8c507a9fc2501b0fb481e0fbf7f469f
Ayush Jain <aj4ayushjain@gmail.com>
2025-08-16 16:53:11 +0530

    Update getDefaultCurrencyByLocale
    Signed-off-by: Ayush Jain <aj4ayushjain@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -35,12 +35,19 @@ function AmortizationCalculator() {
   ];
 
   function getDefaultCurrencyByLocaleOrTimezone(locale: string, timeZone: string): string {
+
     if (timeZone === 'Asia/Kolkata' || locale.startsWith('en-IN')) return 'INR';
-    if (timeZone.startsWith('America/') || locale.startsWith('en-US') ) return 'USD';
     if (timeZone === 'Europe/London' || locale.startsWith('en-GB')) return 'GBP';
     if (timeZone === 'Asia/Tokyo' || locale.startsWith('ja-JP') ) return 'JPY';
     if (timeZone.startsWith('Europe/Moscow') || locale.startsWith('ru-RU') ) return 'RUB';
     if (timeZone === 'Europe/Paris' ||locale.startsWith('fr-FR')) return 'EUR';
+    if (timeZone === 'Australia/Sydney' || locale.startsWith('en-AU') ) return 'AUD';
+    if (timeZone.startsWith('Canada/') || locale.startsWith('en-CA') ) return 'CAD';
+    if (timeZone === 'Asia/Singapore' || locale.startsWith('en-SG') ) return 'SGD';
+    if (timeZone === 'Asia/Shanghai' || locale.startsWith('zh-CN') ) return 'CNY';
+    if (timeZone === 'Africa/Johannesburg' || locale.startsWith('en-ZA') ) return 'ZAR';
+    if (timeZone.startsWith('America/') || locale.startsWith('en-US') ) return 'USD';
+
     // Add more as needed
     return 'INR'; // fallback
   }
